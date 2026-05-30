092ab7be30d21a9fe3c71f60d61d0809ce518021
Mohit Pargaie <mohit.p@osmosys.co>
2026-05-30 18:11:22 +0530

    end of section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,21 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import {Home} from "../features/home/home";
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
-
   protected readonly title = 'Dating App';
   protected members = signal<any>([]);
 
   async ngOnInit(){
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
