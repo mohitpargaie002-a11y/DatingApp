995609301a99173ed22b12c27c560ccbfd60c48f
Mohit Pargaie <mohit.p@osmosys.co>
2026-06-03 18:15:28 +0530

    feat: end of section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+
+      try {
+        return lastValueFrom(initService.init());
+      } finally {
+        const splash = document.getElementById('initial-splash');
+        if (splash) {
+          splash.remove();
+        }
+      }
+    }),
+  ],
 };
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,41 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
-import {Home} from "../features/home/home";
-import { AccountService } from '../core/services/account-service';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Nav } from '../layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = 'Dating App';
-  protected members = signal<any>([]);
-
-  async ngOnInit(){
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try{
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
-    }catch(error){
-      console.log(error);
-      throw error;
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
